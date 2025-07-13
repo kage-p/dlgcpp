83490477e37fe688dbfc4c31fbb163ca18e63e8b
KaGe <kgpsoftware@sky.com>
2025-07-13 11:39:37 +0100

    Extended controls feature
    - ToolBar added missing IToolBar

--- include/dlgcpp/controls/toolbar.h
+++ include/dlgcpp/controls/toolbar.h
@@ -7,18 +7,33 @@ namespace dlgcpp
 {
     namespace controls
     {
-        class ToolBar : public dlgcpp::Control
+        class IToolBar
+        {
+        public:
+            virtual const Size& buttonSize() const = 0;
+            virtual void buttonSize(const Size& value) = 0;
+            virtual const Size& imageSize() const = 0;
+            virtual void imageSize(const Size& value) = 0;
+            virtual const std::vector<ISharedToolBarItem>& items() const = 0;
+            virtual void items(const std::vector<ISharedToolBarItem>& items) = 0;
+        };
+
+        typedef std::shared_ptr<IToolBar> ISharedToolBar;
+
+        class ToolBar :
+            public dlgcpp::Control,
+            public IToolBar
         {
         public:
             explicit ToolBar(const Position& p = Position());
             ~ToolBar() override;
 
-            const Size& buttonSize() const;
-            void buttonSize(const Size& value);
-            const Size& imageSize() const;
-            void imageSize(const Size& value);
-            const std::vector<ISharedToolBarItem>& items() const;
-            void items(const std::vector<ISharedToolBarItem>& items);
+            const Size& buttonSize() const override;
+            void buttonSize(const Size& value) override;
+            const Size& imageSize() const override;
+            void imageSize(const Size& value) override;
+            const std::vector<ISharedToolBarItem>& items() const override;
+            void items(const std::vector<ISharedToolBarItem>& items) override;
 
         private:
             struct toolbar_props* _props;
